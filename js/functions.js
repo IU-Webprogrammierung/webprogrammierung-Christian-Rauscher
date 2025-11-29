29f711cddba1e9208ecdfcefacb5a810be997ac2
ChristianRauscher <christian.rauscher@iu-study.org>
2025-11-29 16:56:22 +0100

    feat: scroll to top button
    add a button to the index page when scrolling down to get to the top of the page again

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,19 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
     }
+
+// Scroll to Top Button
+let mybutton = document.getElementById("scroll-to-top-button");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
